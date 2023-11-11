ef4f69b9aeee6410c69a6fbe8d0daa21345074ad
João Pedro Carvalho <jpdicarvalho01@gmail.com>
2023-11-10 21:27:34 -0300

    arrumado

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 //importando as bibliotecas de sessão e cookies
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser');
 
 //inicializar o express
 const app = express();
@@ -19,6 +20,8 @@ app.use(
         saveUninitialized: true, // salva sessões não inicializadas 
     })
 )
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const produtos = [
     { id: 1, nome: 'Arroz', preco: 25 },
@@ -51,75 +54,122 @@ app.get('/adicionar/:id', (req, res) => {
     const produto = produtos.find((p) => p.id === id);
 
     if (produto) {
-        if (!req.session.carrinho) {
-            req.session.carrinho = [];
+        // Verifique se há um usuário logado
+        if (req.session.userId) {
+            // Verifique se o carrinho do usuário já está inicializado
+            if (!req.session.carrinho) {
+                req.session.carrinho = [];
+            }
+            // Adicione o produto ao carrinho do usuário
+            req.session.carrinho.push(produto);
+        } else {
+            // Se não houver usuário logado, você pode tratar de outra forma ou redirecionar para a página de login
+            res.send('Usuário não está logado.');
+            return;
         }
-        req.session.carrinho.push(produto);
     }
 
-    res.redirect('/produtos')
+    res.redirect('/produtos');
 });
+
 
 //Rota do carrinho
 
 app.get('/carrinho', (req, res) => {
-    const carrinho = req.session.carrinho || [];
-    const total = carrinho.reduce((acc, produto) => acc + produto.preco, 0);
+    const userId = req.session.userId;
 
+    // Verifique se há um usuário logado
+    if (userId) {
+        // Obtenha o carrinho específico do usuário a partir da sessão
+        const carrinho = req.session.carrinho || [];
+        const total = carrinho.reduce((acc, produto) => acc + produto.preco, 0);
+
+        res.send(`
+            <h1>Carrinho de Compras</h1>
+            <ul>
+            ${carrinho.map(
+            (produto) => `<li>${produto.nome} - ${produto.preco}</li>`
+        ).join("")}
+            </ul>
+            <p>Total: ${total}</p>
+            <a href="/produtos">Continuar Comprando</a>
+        `);
+    } else {
+        // Se não houver usuário logado, você pode tratar de outra forma ou redirecionar para a página de login
+        res.send('Usuário não está logado.');
+    }
+});
+
+
+app.get('/login', (req, res) => {
     res.send(`
-        <h1>Carrinho de Compras</h1>
-        <ul>
-        ${carrinho.map(
-        (produto) => `<li>${produto.nome} - ${produto.preco}</li>`
-    ).join("")
-        }
-    </ul>
-    <p>Total: ${total}</p>
-    <a href="/produtos">Continuar Comprando</a>
+        <h1>Login</h1>
+        <form method="post" action="/login">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required><br>
+
+            <label for="senha">Senha:</label>
+            <input type="password" id="senha" name="senha" required><br>
+
+            <button type="submit">Login</button>
+        </form>
     `);
 });
 
+// Rota para processar o formulário de login
 app.post('/login', (req, res) => {
     const { email, senha } = req.body;
 
     const usuario = usuarios.find((user) => user.email === email && user.senha === senha);
 
     if (usuario) {
+        // Verifique se o carrinho já está presente na sessão
+        req.session.userId = usuario.id;
 
-        res.cookie('userId', usuario.id, { maxAge: 900000, httpOnly: true });
-        res.send('Login realizado com sucesso!');
+        if (!req.session.carrinho) {
+            req.session.carrinho = [];
+        }
+
+        res.redirect('/perfil');
     } else {
         res.send('Credenciais inválidas. Tente novamente.');
     }
 });
 
+
+
 app.get('/perfil', (req, res) => {
-    const userId = req.cookies.userId;
+    const userId = req.session.userId;
 
     if (userId) {
         const usuario = usuarios.find((user) => user.id === parseInt(userId, 10));
 
         if (usuario) {
             res.send(`
-          <h1>Perfil do Usuário</h1>
-          <p>ID: ${usuario.id}</p>
-          <p>Nome: ${usuario.nome}</p>
-          <p>Email: ${usuario.email}</p>
-          <a href="/logout">Sair</a>
-        `);
+              <h1>Perfil do Usuário</h1>
+              <p>ID: ${usuario.id}</p>
+              <p>Nome: ${usuario.nome}</p>
+              <p>Email: ${usuario.email}</p>
+              <a href="/produtos">Ir para Produtos</a>
+              <a href="/logout">Sair</a>
+            `);
         } else {
             res.send('Usuário não encontrado.');
         }
     } else {
-        res.send('Usuário não está logado!');
+        // Redirecionar para a página de login se o usuário não estiver logado
+        res.redirect('/login');
     }
 });
 
 
+
 app.get('/logout', (req, res) => {
-    res.clearCookie('userId');
-    res.redirect('/perfil');
+    req.session.destroy(); // Limpa todos os dados da sessão
+    res.clearCookie('userId'); // Limpa o cookie do usuário
+    res.redirect('/login');
 });
+
 
 
 app.listen(3000, () => {
